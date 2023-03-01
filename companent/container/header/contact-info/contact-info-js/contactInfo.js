58ba39cbf591ba399971352fea1307de4151838c
KanoplichAndrey <kanop0000@gmail.com>
2023-03-01 20:21:37 +0300

    2

--- companent/container/header/contact-info/contact-info-js/contactInfo.js
+++ companent/container/header/contact-info/contact-info-js/contactInfo.js
@@ -8,7 +8,7 @@ if (animItems.length > 0) {
       const animItem = animItems[index];
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
-      const animStart = 9;
+      const animStart = 4;
 
       let animInemPoint = window.innerHeight - animItemHeight / animStart;
 
